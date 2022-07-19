47306f6ca47377e92d695e9a3ae0b2ac48ec5058
wkkang-t <kaikang_wong@mymail.sutd.edu.sg>
2022-07-19 13:29:54 +0800

    first commit

---
+++ contracts/Adoption.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+contract Adoption {
+    address[16] public adopters;
+
+    // Adopting a pet
+    function adopt(uint petId) public returns (uint) {
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
+
+    // Retrieving the adopters
+    function getAdopters() public view returns (address[16] memory) {
+        return adopters;
+    }
+}
